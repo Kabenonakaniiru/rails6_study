f3660f2384c951801557ab745642177fb3bbf466
kabenonakaniiru <uhotayori@gmail.com>
2020-10-03 23:59:44 +0900

    Action Mailerの送信設定を追記。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,20 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #Action Mailer URL
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@example.com'} # FIXME: いつか変更
+  # see https://railsguides.jp/action_mailer_basics.html
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'example.com',
+  #   user_name:            '<ユーザー名>',
+  #   password:             '<パスワード>',
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true
+  # }
 end
