12716da727e3f918d86638687172a6ccb012ae15
kabenonakaniiru <uhotayori@gmail.com>
2020-10-05 23:16:02 +0900

    ドロップダウン実装。右寄せにする必要がある。

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,12 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+// FIXME: 一時的に無効に戻した。packnameを指定しての読み込み方を調べること。(タグも変える必要がある)
 // see https://github.com/rails/webpacker/blob/master/docs/webpack.md#add-splitchunks-webpack-v4
 // Enable the default config
-environment.splitChunks()
+// environment.splitChunks()
 // or using custom config
-environment.splitChunks((config) => Object.assign({}, config, { optimization: { splitChunks: false } }))
+// environment.splitChunks((config) => Object.assign({}, config, { optimization: { splitChunks: false } }))
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
