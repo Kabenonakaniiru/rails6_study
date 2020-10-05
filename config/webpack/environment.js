e0a8f094ee6418a87d6ba8a271d5b8368d5d9a05
kabenonakaniiru <uhotayori@gmail.com>
2020-10-05 22:19:31 +0900

    split chunksの設定を追加。

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,11 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+// see https://github.com/rails/webpacker/blob/master/docs/webpack.md#add-splitchunks-webpack-v4
+// Enable the default config
+environment.splitChunks()
+// or using custom config
+environment.splitChunks((config) => Object.assign({}, config, { optimization: { splitChunks: false } }))
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
