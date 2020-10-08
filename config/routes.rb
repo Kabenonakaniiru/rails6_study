fda919413285fdd615b5bc3fd67514d0b8b0508d
kabenonakaniiru <uhotayori@gmail.com>
2020-10-08 23:36:18 +0900

    devise:管理者スコープを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # FIXME: 独自コントローラ用
-  # Rails.application.routes.draw do
-  #   devise_for :users, controllers: {
-  #     sessions: 'users/sessions'
-  #   }
-  # end
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
 end
