fa46eef3d343d66387db9582f8c89bb13ef3f9d3
kabenonakaniiru <uhotayori@gmail.com>
2020-10-15 21:50:02 +0900

    登録までの流れをとりあえず実装(動作未確認

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,12 +1,31 @@
 class MissionsController < ApplicationController
   before_action :classifications
 
+  def index
+    @missions = Mission.all.includes(:classification)
+  end
+
   def new
     @mission = Mission.new
+  end
+
+  def create
+    # TODO: ModelViewへの切り出し。
+    # TODO: Validationの実装
+    Mission.create!(mission_params)
   end
 
   private
     def classifications
       @classification = Classification.all
+    end
+
+    def mission_params
+      params.require(:mission).permit(
+        :name,
+        :classification_id,
+        :limited,
+        :url,
+        :note)
     end
 end
