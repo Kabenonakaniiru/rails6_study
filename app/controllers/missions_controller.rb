8f7fef0bc5718b36b97108c842dc162fdad9175d
kabenonakaniiru <uhotayori@gmail.com>
2020-10-15 21:39:18 +0900

    formの大体の大枠を追加(新規登録側

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,2 +1,12 @@
 class MissionsController < ApplicationController
+  before_action :classifications
+
+  def new
+    @mission = Mission.new
+  end
+
+  private
+    def classifications
+      @classification = Classification.all
+    end
 end
