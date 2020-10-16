9d2d788a545ab6bea4dd9847f33b9e9090903066
kabenonakaniiru <uhotayori@gmail.com>
2020-10-16 23:00:16 +0900

    更新削除処理追加。基底クラスに追加したメッセージメソッドを利用するように修正。

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -5,17 +5,50 @@ class MissionsController < ApplicationController
     @missions = Mission.all.includes(:classification)
   end
 
+  def edit
+    @mission = Mission.find(params[:id])
+  end
+
   def create
     # TODO: ModelViewへの切り出し。
     # TODO: Validationの実装
     Mission.create!(mission_params)
-    redirect_to missions_url, notice: '任務マスタの登録に成功しました。'
+    redirect_to missions_url, notice: mission_manipulate_message('登録', true)
   rescue => e
     # TODO: エラー表示の追加
     render action: :new
   end
 
+  def update
+    ActiveRecord::Base.transaction do
+      # TODO: ModelViewへの切り出し。
+      # TODO: Validationの実装
+      Mission.update!(mission_params)
+      redirect_to missions_url, notice: mission_manipulate_message('更新', true)
+    end
+  rescue => e
+    # TODO: エラー表示の追加
+    render action: :edit
+  end
+
+
+  def destroy
+    ActiveRecord::Base.transaction do
+      # TODO: ModelViewへの切り出し。
+      # TODO: Validationの実装
+      Mission.find(params[:id])&.update!(mission_params)
+      redirect_to missions_url, notice: mission_manipulate_message('削除', true)
+    end
+  rescue => e
+    # TODO: エラー表示の追加
+    render action: :index
+  end
+
   private
+    def mission_manipulate_message(manipulate, result)
+      mamnipulate_message('任務マスタ', manipulate, result)
+    end
+
     def classifications
       @classifications = Classification.all
     end
