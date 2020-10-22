6075312860c12784d2f240bdec5283ac6e04468f
kabenonakaniiru <uhotayori@gmail.com>
2020-10-23 00:06:34 +0900

    ひとまずjQueryでイメージ実装

--- app/javascript/packs/missions/index.js
+++ app/javascript/packs/missions/index.js
@@ -1,1 +1,8 @@
-alert("!");
+(function ($) {
+  $(function () {
+    $('.edit-row').on('click', function () {
+      // とりあえず切り替えだけの処理(本当は判定文がいる) FIXME:
+      $(this).removeClass('fa-edit').addClass('fa-save');
+    });
+  });
+})(jQuery);
