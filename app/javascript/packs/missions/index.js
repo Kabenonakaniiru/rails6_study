a59cb54c2e664bc0e3098bc69867b3376f823063
kabenonakaniiru <uhotayori@gmail.com>
2020-10-27 01:30:49 +0900

    一旦あるべきコードをコミット。(まだ反転まで出来てない)

--- app/javascript/packs/missions/index.js
+++ app/javascript/packs/missions/index.js
@@ -1,8 +1,16 @@
 (function ($) {
   $(function () {
     $('.edit-row').on('click', function () {
-      // とりあえず切り替えだけの処理(本当は判定文がいる) FIXME:
-      $(this).removeClass('fa-edit').addClass('fa-save');
+      // とりあえず切り替えだけの処理
+      var $this = $(this);
+      var dataIconClass;
+      // if ($this.attr('data-icon') === 'edit') {
+      //   dataIconClass = 'fa-edit';
+      // } else {
+      //   dataIconClass = 'fa-save';
+      // }
+      // var icon = $this.find('[data-fa-i2svg]');
+      $(this).toggleClass('fa-save').toggleClass('fa-edit');
     });
   });
 })(jQuery);
