72ea4ac20272a98a1e4667b626f145af47080614
kabenonakaniiru <uhotayori@gmail.com>
2020-10-28 02:07:39 +0900

    現状のソースを一旦コミット

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,11 @@ import "bootstrap"
 import "bootstrap/scss/bootstrap.scss"
 import "@fortawesome/fontawesome-free/js/all.js";
 import "@fortawesome/fontawesome-free/css/all.css";
+import { config } from '@fortawesome/fontawesome-svg-core'
+console.log(config.autoReplaceSvg)
+config.autoReplaceSvg = 'nest';
+console.log(config.autoReplaceSvg)
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
