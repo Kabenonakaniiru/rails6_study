ad152c9d3a78abfb81ec2294090918cb0ce4d281
kabenonakaniiru <uhotayori@gmail.com>
2020-11-03 06:32:09 +0900

    アイコン切り替えに成功

--- app/javascript/packs/missions/index.js
+++ app/javascript/packs/missions/index.js
@@ -1,16 +1,11 @@
 (function ($) {
   $(function () {
-    $('.edit-row').on('click', function () {
+    $('.edit-row-icon').on('click', function () {
       // とりあえず切り替えだけの処理
-      var $this = $(this);
-      var dataIconClass;
-      // if ($this.attr('data-icon') === 'edit') {
-      //   dataIconClass = 'fa-edit';
-      // } else {
-      //   dataIconClass = 'fa-save';
-      // }
-      // var icon = $this.find('[data-fa-i2svg]');
-      $(this).toggleClass('fa-save').toggleClass('fa-edit');
+      $(this)
+        .find('[data-fa-i2svg]')
+        .toggleClass('fa-edit')
+        .toggleClass('fa-save');
     });
   });
 })(jQuery);
