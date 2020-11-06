90ae565ce88c974f83df8cd79652b18cb4e0f960
kabenonakaniiru <uhotayori@gmail.com>
2020-11-06 23:57:28 +0900

    編集、保存アイコンをクリックすると、それに合わせて行の編集可能、不能状態が切り替わるように修正

--- app/javascript/packs/missions/index.js
+++ app/javascript/packs/missions/index.js
@@ -2,10 +2,18 @@
   $(function () {
     $('.edit-row-icon').on('click', function () {
       // とりあえず切り替えだけの処理
-      $(this)
-        .find('[data-fa-i2svg]')
-        .toggleClass('fa-edit')
+      var $this = $(this);
+      var dataFaI2Svg = $this.find('[data-fa-i2svg]');
+      dataFaI2Svg.toggleClass('fa-edit')
         .toggleClass('fa-save');
+      // テキストフィールドとラベル表示を切り替える FIXME: あとでいいコメント考えてくだしあ
+      // this = i → row取得→配下tdの特定クラスを切りかえる
+      var row = $this.parent().parent();
+      // edit→saveアイコンに切り替えるとき、行配下のセルを編集可能にする。
+      row.children('.mission-count-area').each(function (index, td) {
+        $(td).find('.mission-count-display-cell').toggleClass('d-none');
+        $(td).find('.mission-count-editor').toggleClass('d-none');
+      });
     });
   });
 })(jQuery);
