28fb41b9a1d8ef0bed16e1664a8ea0f554f8439a
kabenonakaniiru <uhotayori@gmail.com>
2020-11-09 22:20:01 +0900

    不要にjQueryオブジェクトを二度作っていたので、一度作ってキャッシュするように修正

--- app/javascript/packs/admins/missions/index.js
+++ app/javascript/packs/admins/missions/index.js
@@ -11,8 +11,9 @@
       var row = $this.parent().parent();
       // edit→saveアイコンに切り替えるとき、行配下のセルを編集可能にする。
       row.children('.mission-count-area').each(function (index, td) {
-        $(td).find('.mission-count-display-cell').toggleClass('d-none');
-        $(td).find('.mission-count-editor').toggleClass('d-none');
+        var $td = $(td);
+        $td.find('.mission-count-display-cell').toggleClass('d-none');
+        $td.find('.mission-count-editor').toggleClass('d-none');
       });
     });
   });
