7290bc038a2307d91b09f0d43195a6c7fd4885f4
kabenonakaniiru <uhotayori@gmail.com>
2020-11-14 09:39:41 +0900

    運用想定上、期間が長い方しか運用しないので、任務の生存期間が長い方を上に持ってきた。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Classification.create([{ name: '日次' }, { name: '週次' }, { name: '月次' }, { name: '3ヶ月' }, { name: '年次' }, { name: '単発' }])
+Classification.create([{ name: '単発' }, { name: '年次' }, { name: '3ヶ月' }, { name: '月次' }, { name: '週次' }, { name: '日次' }])
