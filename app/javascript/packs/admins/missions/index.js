3c0253a9a17b6799b3b3998a1f1b4c9798de386e
kabenonakaniiru <uhotayori@gmail.com>
2020-11-20 10:00:57 +0900

    とりあえずtoggle部分を関数化

--- app/javascript/packs/admins/missions/index.js
+++ app/javascript/packs/admins/missions/index.js
@@ -18,12 +18,8 @@
           errorFunc();
         });
     };
-    $('.edit-row-icon').on('click', function () {
-      // とりあえず切り替えだけの処理
-      var $this = $(this);
-      var dataFaI2Svg = $this.find('[data-fa-i2svg]');
-      dataFaI2Svg.toggleClass('fa-edit')
-        .toggleClass('fa-save');
+    // TODO: 後で適切な引数名に変更すること。更新とそうでないときの処理を分けること。
+    var toggleRow = function ($this, data) {
       // テキストフィールドとラベル表示を切り替える FIXME: あとでいいコメント考えてくだしあ
       // this = i → row取得→配下tdの特定クラスを切りかえる
       var row = $this.parent().parent();
@@ -33,14 +29,22 @@
         $td.find('.mission-count-display-cell').toggleClass('d-none');
         $td.find('.mission-count-editor').toggleClass('d-none');
       });
+    }
+    $('.edit-row-icon').on('click', function () {
+      // とりあえず切り替えだけの処理
+      var $this = $(this);
+      var dataFaI2Svg = $this.find('[data-fa-i2svg]');
+      dataFaI2Svg.toggleClass('fa-edit')
+        .toggleClass('fa-save');
       if (dataFaI2Svg.hasClass('fa-edit')) {
         // TODO: ここに保存時の動作追加
+        toggleRow($this);
       } else {
         // TODO: ここに編集時の動作追加
         var url = $this.parent().find(".row_get_link").attr('href');
         // TODO: 共通化
         commonJsonAjax(url, 'get', {}, function (data) {
-          alert(data);
+          toggleRow($this, data);
         }, function () {
           // TODO: 実装
         });
