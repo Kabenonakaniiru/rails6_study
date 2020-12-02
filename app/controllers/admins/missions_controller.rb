4f969774fecaf4d3e64b4fa4b6af2b6b8f1c89d1
kabenonakaniiru <uhotayori@gmail.com>
2020-12-03 04:42:32 +0900

    ヘッダが間違っていたので修正(編集カラムが海域情報に来てなかった

--- app/controllers/admins/missions_controller.rb
+++ app/controllers/admins/missions_controller.rb
@@ -12,7 +12,8 @@ class Admins::MissionsController < ApplicationController
     parent_sea_areas.each_with_index do |parent_sea_area, i|
       @header_parent_areas.store(parent_sea_area.name, parent_sea_area_counts[i])
     end
-    @total_parent_sea_area_count = parent_sea_area_counts.inject(&:+)
+    # 編集カラム分、1加算している。
+    @total_parent_sea_area_count = parent_sea_area_counts.inject(&:+) + 1
     @header_children_areas = ActiveRecord::Base.connection.select_all('SELECT id, level, parent_area_id, name, column_name FROM areas parent LEFT OUTER JOIN (SELECT id AS child_id, name AS child_name, parent_area_id AS child_parent_area_id FROM areas) child ON parent.id = child.child_parent_area_id WHERE parent.column_name IS NOT NULL;')
     @mission_count_areas = Area.where.not(column_name: nil)
     @missions = Mission.all.includes(:classification)
