3d1dd6b68fc3c6689c16074f1c72e9492b00d560
kabenonakaniiru <uhotayori@gmail.com>
2020-12-18 07:50:12 +0900

    noteにvalidation追加

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,4 +3,5 @@ class Mission < ApplicationRecord
   validates :name, presence: true
   validates :classification_id, presence: true
   validates :url, length: { minimum: 7, maximum: 255 } # TODO: 後で精査(特にURLの長さ)
+  validates :note, length: { maximum: 255 }
 end
